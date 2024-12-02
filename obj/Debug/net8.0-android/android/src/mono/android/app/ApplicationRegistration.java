da5fd172c71a5bd0bdd81feccd8235efff9a1aae
mynumber701 <vlad.v.gladkiy@gmail.com>
2024-12-02 15:03:46 +0200

    Initital commit

---
+++ obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("LABA2.MainApplication, LABA2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6461e408187d61466c.MainApplication.class, crc6461e408187d61466c.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
